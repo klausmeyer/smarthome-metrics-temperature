111738d4b4a1f1ed200dfa369ba0a10cc0a2971b
Klaus Meyer <spam@klaus-meyer.net>
2024-11-30 14:43:51 +0100

    fix ssl issue with fritzbox

--- config.ru
+++ config.ru
@@ -5,6 +5,7 @@ Fritzbox::Smarthome.configure do |config|
   config.username   = ENV['FRITZBOX_USERNAME'] || 'smarthome'
   config.password   = ENV['FRITZBOX_PASSWORD'] || 'verysmart'
   config.verify_ssl = (ENV['FRITZBOX_VERIFY_SSL'] || 'false') == 'true'
+  config.logger     = Logger.new(STDOUT, level: Logger::INFO)
 end
 
 require_relative 'lib/app'
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,9 @@ require 'bundler'
 
 Bundler.require(:default)
 
+# https://stackoverflow.com/questions/76183622/since-a-ruby-container-upgrade-we-expirience-a-lot-of-opensslsslsslerror
+if OpenSSL::SSL.const_defined?(:OP_IGNORE_UNEXPECTED_EOF)
+  OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] |= OpenSSL::SSL::OP_IGNORE_UNEXPECTED_EOF
+end
+
 require_relative '../lib/app'
